b0e0f9bc91b6065340289929e5e383f24f098280
Abhishek <info_tech@smartbits.co.in>
2021-11-23 16:31:09 +0530

    bootstrap version correction

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-import Script from 'next/script';
+import Script from 'next/script'
 import Layout from "../components/Layout";
 // import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
-    <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
-    <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></Script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-    <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></Script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+    <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></Script>
+    <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></Script>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"></Script>
     <Component {...pageProps} />
   </Layout>
   );
