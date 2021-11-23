1f6e87db0b3876bce364f19280626d24e5338ab1
Abhishek <info_tech@smartbits.co.in>
2021-11-23 14:54:42 +0530

    jquery script load

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
   return (
   <Layout>
-    <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></Script>
     <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
+    <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></Script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
     <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></Script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
